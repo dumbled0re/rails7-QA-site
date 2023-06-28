f9ce3c235fac68ec544fa17911576424e8086e71
dumbled0re <smu315064@gmail.com>
2023-06-28 20:56:46 +0900

    feat: 78_class_const.rb

---
+++ basic_ruby/78_class_const.rb
@@ -0,0 +1,36 @@
+class Car
+    REGION = "USA"
+    # クラス変数は@@が2個
+    @@count = 0
+
+    def initialize(name)
+        @name = name
+        @@count += 1
+    end
+
+    def hello
+        puts "Hello! I am #{@name}. #{@@count} instance(s)."
+    end
+
+    # クラスメソッド(selfが必要)
+    def self.info
+        puts "#{@@count} instance(s). REGION: #{REGION}"
+    end
+end
+
+# インスタンス作成
+Kitt = Car.new('Kitt')
+# クラスメソッドはインスタンスを生成しなくても呼べる
+Car.info
+# Kitt.hello
+
+karr = Car.new("Karr")
+Car.info
+# karr.hello
+
+nakamura = Car.new("Nakamura")
+Car.info
+# nakamura.hello
+
+# クラスの定数にアクセス
+puts Car::REGION
